17732b7c30a8e0d8683da154a4fd9525d72458e7
Norddin <n.oubahman@hotmail.com>
2020-11-26 15:34:05 +0100

    custom AppPicker and Textinput components

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
   StatusBar,
   TextInput,
   Text,
+  Switch,
 } from "react-native";
 import {
   useDimensions,
@@ -18,26 +19,24 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppText from "./app/components/AppText";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+
+import Screen from "./app/components/Screen";
+import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
   //console.log(useDimensions());
 
   const [firstName, setFirstName] = useState("");
-
+  const [isNew, setIsNew] = useState(false);
   return (
     <Screen>
-      <AppTextInput placeholder="username" icon="email" />
+      <AppPicker icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
